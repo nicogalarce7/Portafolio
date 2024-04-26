490d93bc036ccc757a2da4bb020663c65c5bf537
Nicolas Galarce <nicogalarce7@gmail.com>
2024-04-26 20:36:43 +0200

    Last errors css fixed

--- js/app.js
+++ js/app.js
@@ -9,3 +9,32 @@ function toogleMenu(){
 
 OpenMenuBtn.addEventListener("click", toogleMenu);
 CloseMenuBtn.addEventListener("click", toogleMenu);
+
+const menuLinks = document.querySelectorAll('.menu a[href^="#"]');
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      const id = entry.target.getAttribute("id");
+      const menuLink = document.querySelector(`.menu a[href="#${id}"]`);
+
+      if (entry.isIntersecting) {
+        document.querySelector(".menu a.selected").classList.remove("selected");
+        menuLink.classList.add("selected");
+      }
+    });
+  },
+  { rootMargin: "-30% 0px -70% 0px" }
+);
+
+menuLinks.forEach((menuLink) => {
+  menuLink.addEventListener("click", function () {
+    menu.classList.remove("menu_opened");
+  });
+
+  const hash = menuLink.getAttribute("href");
+  const target = document.querySelector(hash);
+  if (target) {
+    observer.observe(target);
+  }
+});
